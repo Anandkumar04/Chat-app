9188c28c1e96e55dadbd2978f270ec423d849256
Anand kumar Ketha <146409586+Anandkumar04@users.noreply.github.com>
2025-05-31 19:10:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-phi-five-89.vercel.app/",
     methods: ["GET", "POST"]
   }
 });
